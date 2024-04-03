4bc4958c74606594dc2fbea6a11d02dd493676ca
Omkar <omkar111pawar@gmail.com>
2024-04-03 19:15:56 +0530

    added CRUD operation using sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,28 +14,31 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product])=>{
-    res.render('shop/product-detail', {
-      product: product[0],
-      pageTitle: product.title,
-      path: '/products'
-    });
-  })
-  .catch(err=>console.log(err));
+  Product.findByPk(prodId)
+    .then(product=>{
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err=>console.log(err));
 
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows,fieldData])=>{
+  Product.findAll()
+    .then(product=>{
       res.render('shop/index', {
-        prods: rows,
+        prods: product,
         pageTitle: 'Shop',
         path: '/'
       });
     })
-    .catch(err=>console.log(err));
+    .catch(err=>{
+      console.log(err)
+    })
+  
   
 };
 
